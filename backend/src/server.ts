9f9f690f26906a8a7974481baf69318a7b5b26cc
Markus Rutström <markusrutstrom@gmail.com>
2022-01-09 18:36:02 +0100

    Loggin middleware i nbackend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,65 +1,21 @@
 import dotenv from "dotenv";
-import express from "express"
-import cors from "cors"
+import express, { RequestHandler } from "express";
+import cors from "cors";
 import { graphqlHTTP } from "express-graphql";
-import { buildSchema } from "graphql";
-import { RandomDie } from "./RandomDie";
+import { root, schema } from "./GraphQlExample";
 
 dotenv.config({
-  path: ".env"
-})
+  path: ".env",
+});
 
-var schema = buildSchema(`
-  type RandomDie {
-    numOfSides: Int!
-    rollOnce: Int!
-    roll(numOfRolls: Int!): [Int]
-  }
-  type Query {
-    hello(name: String!): String
-    quoteOfTheDay: String
-    random: Float!
-    rollDice(numOfDice: Int!, numSides: Int): [Int]
-    getDie(numOfSides: Int): RandomDie
-  }
-`);
-
-
-type HelloRequestArgs = {
-  name: String
-}
-type RollDiceArgs = {
-  numOfDice: number,
-  numSides?: number
-}
-type GetDieArgs = {
-  numOfSides?: number,
-}
-
-var root = {
-  hello: (args: HelloRequestArgs) => {
-    return `Hello ${args.name}!`;
-  },
-  quoteOfTheDay: () => {
-    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
-  },
-  random: () => {
-    return Math.random();
-  },
-  rollDice: (args: RollDiceArgs) => {
-    var output = [];
-    for (var i = 0; i < args.numOfDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (args.numSides || 6)));
-    }
-    return output;
-  },
-  getDie: (args: GetDieArgs) => {
-    return new RandomDie(args.numOfSides || 6);
-  }
+const loggingMiddleware: RequestHandler = (req, res, next) => {
+  console.log(`Request from ${req.ip}, Response status code ${res.statusCode}`);
+  next();
 };
 
 var app = express();
 app.use(cors());
+app.use(loggingMiddleware);
 
 app.use(
   "/graphql",
@@ -73,4 +29,4 @@ app.use(
 const port = process.env.APP_PORT || 5000;
 app.listen(port, () => {
   console.log("Server started on port", port);
-})
+});
