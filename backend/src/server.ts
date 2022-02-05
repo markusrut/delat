d00c1f6b5ef8a1fe92339261593b9945d5a90bd6
Markus Rutström <markusrutstrom@gmail.com>
2022-02-05 10:41:36 +0100

    start adding jwt token auth

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
+import jwtMiddleware from "express-jwt";
+import jwt from "jsonwebtoken";
 import cors from "cors";
 import { graphqlHTTP } from "express-graphql";
 // import { schema } from "./api-schema";
@@ -12,8 +14,26 @@ dotenv.config({
 var server = express();
 server.use(cors());
 
+const jwtAlgorithm = "HS256";
+const jwtOptions = {
+  secret: process.env.JWT_SECRET ?? "MissingSecret",
+  algorithms: [jwtAlgorithm],
+};
+server.use(jwtMiddleware(jwtOptions).unless({ path: ["/health", "/token"] }));
+
+server.use("/token", (req, res) => {
+  const token = jwt.sign({ foo: "bar" }, jwtOptions.secret, {
+    algorithm: jwtAlgorithm,
+  });
+  res.json({ token });
+});
+
 server.use("/health", (_req, res) => {
   res.status(200).json({ uptime: process.uptime() });
+});
+server.use("/me", (_req, res) => {
+  const user = _req.user ?? {};
+  res.status(200).json({ user });
 });
 
 server.use(
@@ -28,4 +48,5 @@ server.use(
 const port = process.env.APP_PORT || 5000;
 server.listen(port, () => {
   console.log("Server started on port", port);
+  console.log(`http://localhost:${port}`);
 });
