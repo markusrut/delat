77b918ae5c6a325ba0a3e3bf78dab6f430023854
Markus Rutström <markusrutstrom@gmail.com>
2022-03-20 21:02:11 +0100

    hot reload theme

--- flutter_app/lib/theme.dart
+++ flutter_app/lib/theme.dart
@@ -1,35 +1,35 @@
 import 'package:flutter/material.dart';
 
 class CustomTheme {
-  static final ThemeData _base = ThemeData(
-    primarySwatch: Colors.purple,
-    textTheme: const TextTheme(
-      headline1: TextStyle(
-        fontSize: 72.0,
-        fontWeight: FontWeight.bold,
-      ),
-      headline4: TextStyle(
-        fontSize: 24,
-        fontWeight: FontWeight.bold,
-      ),
-    ),
-  );
+  static ThemeData get _base => ThemeData(
+        primarySwatch: Colors.purple,
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            fontSize: 72.0,
+            fontWeight: FontWeight.w800,
+          ),
+          headline4: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      );
 
-  static ThemeData light = _base.copyWith(
-    scaffoldBackgroundColor: Colors.deepPurple.shade100,
-    textTheme: const TextTheme(
-      headline1: TextStyle(
-        color: Color.fromARGB(255, 235, 0, 235),
-      ),
-    ),
-  );
+  static ThemeData get light => _base.copyWith(
+        scaffoldBackgroundColor: Colors.deepPurple.shade100,
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            color: Color.fromARGB(255, 235, 0, 235),
+          ),
+        ),
+      );
 
-  static ThemeData dark = _base.copyWith(
-    scaffoldBackgroundColor: Colors.deepPurple.shade900,
-    textTheme: const TextTheme(
-      headline1: TextStyle(
-        color: Color.fromARGB(255, 65, 1, 65),
-      ),
-    ),
-  );
+  static ThemeData get dark => _base.copyWith(
+        scaffoldBackgroundColor: Colors.deepPurple.shade900,
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            color: Color.fromARGB(255, 65, 1, 65),
+          ),
+        ),
+      );
 }
